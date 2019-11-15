0eef742ce5025efe57d7bf8ffc02c962cb04cee2
unknown <helene.carlier.gubler@gmail.com>
2019-11-15 19:51:46 +0100

    code HTML

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,25 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    clients : [
+      {id : 1, nom : "Bob"},
+      {id : 2, nom : "Martine"},
+      {id : 3, nom : "Jeanne"}
+    ]
+  }
+
+  render() {
+      return(
+        <div>
+        <h1>Liste de client</h1>
+        <ul>
+          <li>Bob <button>X</button></li>
+          <li>Martine <button>X</button></li>
+          <li>Jeanne <button>X</button></li>
+        </ul>
+        <input type="text" placeholder="Ajouter un client"></input>
+        <button>Confirmer</button>
+        </div>
+      )
+
+  }
+
 }
 
 export default App;
